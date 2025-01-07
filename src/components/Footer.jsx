547ace3107732d67b1f662df8869a4b3c6beb472
Afsal Ka <afsalafzz63@gmail.com>
2025-01-07 08:40:37 +0530

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,14 +9,14 @@ const Footer = () => {
   return (
     <div className="px-[16px] md:px-[40px] lg:px-[80px] pt-[44px] bg-white text-nowrap">
       <div className="flex w-full justify-between flex-col lg:flex-row gap-[50px]">
-        <div className="max-w-[350px] flex flex-col gap-7">
+        <div className="max-w-full flex flex-col gap-7">
           <div className="">
             <p className="text-[40px] text-black font-[thin]">Let’s Connect</p>
             <p className="text-[38px] text-black/65 font-medium leading-none xs:text-[48px]">
               bd@harisand.co
             </p>
           </div>
-          <div className="flex gap-3 sm:gap-5 md:gap-6">
+          <div className="flex gap-4 sm:gap-5 md:gap-6">
             <div className="border-[1.5px] border-black rounded-full p-[18.5px] sm:p-[21px] w-[65px] sm:w-[70px] h-[65px] sm:h-[70px]">
               <img src={facebookIcon} alt="facebook Icon" className="max-w-max"/>
             </div>
@@ -31,10 +31,10 @@ const Footer = () => {
             </div>
           </div>
         </div>
-        <div className=" items-start">
-          <div className="flex w-fit flex-col items-start lg:items-end">
-            <div className="w-fit h-full flex  ">
-              <img className="h-full w-[350px] md:w-[500px] lg:w-[500px] xl:w-[870px]" src={Logo} alt="Haris&Co" />
+        <div className=" items-start w-full">
+          <div className="flex w-full flex-col items-start lg:items-end">
+            <div className="w-full h-full flex  ">
+              <img className="h-full w-full" src={Logo} alt="Haris&Co" />
             </div>
             <div className="flex justify-evenly md:justify-between gap-[35px] md:gap-[50px]  lg:gap-x-[60px] lg:gap-y-[30px]  mt-[40px] *:font-[thin] w-full flex-wrap">
               <p className="text-[18px]">Services</p>
